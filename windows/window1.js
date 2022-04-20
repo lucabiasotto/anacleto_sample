31803e7c3265e7916edf7f9f6f585f475bc3b704
Luca <luca.biasotto.89@gmail.com>
2022-04-20 22:44:59 +0200

    add finestre

---
+++ windows/window1.js
@@ -0,0 +1,77 @@
+{
+    "window": "window1",
+    "items": [
+        {
+            "id": "home-pnl",
+            "title": "WINDOW 1",
+            "width": "col-12",
+            "background": "bg-primary-reverse",
+            "isCard": true
+        },
+        {
+            "id": "abc",
+            "title": "Pannello 1",
+            "width": [
+                "col-12",
+                "col-12",
+                "col-6",
+                "lg:col-4",
+                "lg:col-4"
+            ],
+            "background": "bg-primary",
+            "isCard": true
+        },
+        {
+            "id": "def",
+            "title": "Pannello 2",
+            "width": [
+                "col-12",
+                "col-12",
+                "col-6",
+                "lg:col-4",
+                "lg:col-4"
+            ],
+            "background": "bg-primary",
+            "isCard": true
+        },
+        {
+            "id": "ghi",
+            "title": "Pannello 3",
+            "width": [
+                "col-12",
+                "col-12",
+                "col-6",
+                "lg:col-4",
+                "lg:col-4"
+            ],
+            "background": "bg-primary",
+            "isCard": true
+        },
+        {
+            "id": "lmn",
+            "type": "form",
+            "title": "Pannello 4",
+            "width": "col-12",
+            "background": "bg-primary-reverse",
+            "isCard": false,
+            "items": [
+                {
+                    "type": "textInput",
+                    "value": "Prova",
+                    "width": "w-2",
+                    "bgColor": "bg-primary",
+                    "margin": "m-4",
+                    "id": "ti1",
+                    "disabled": false,
+                    "placeholder": "Placeholder"
+                },
+                {
+                    "type": "switchInput",
+                    "checked": true,
+                    "id": "t12",
+                    "disabled": false
+                }
+            ]
+        }
+    ]
+}
---
+++ windows/window2.js
@@ -0,0 +1,51 @@
+{
+    "window": "window2",
+    "items": [
+        {
+            "id": "home-pnl",
+            "title": "WINDOW 2",
+            "width": "col-12",
+            "background": "bg-primary-reverse",
+            "isCard": false
+        },
+        {
+            "id": "abc",
+            "title": "Pannello 1",
+            "width": [
+                "col-12",
+                "col-12",
+                "col-6",
+                "lg:col-4",
+                "lg:col-4"
+            ],
+            "background": "bg-primary",
+            "isCard": true
+        },
+        {
+            "id": "def",
+            "title": "Pannello 2",
+            "width": [
+                "col-12",
+                "col-12",
+                "col-6",
+                "lg:col-4",
+                "lg:col-4"
+            ],
+            "background": "bg-primary",
+            "isCard": true
+        },
+        {
+            "id": "ghi",
+            "title": "Pannello 3",
+            "width": [
+                "col-12",
+                "col-12",
+                "col-6",
+                "lg:col-4",
+                "lg:col-4"
+            ],
+            "background": "bg-primary",
+            "isCard": true
+        }
+    ]
+}
