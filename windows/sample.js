2cbede5c6e65828eef79e94a1a0daa9d86168904
Luca Biasotto <luca.biasotto.89@gmail.com>
2022-04-28 21:38:31 +0200

    Luca Biasotto update file windows/sample.js

--- windows/sample.js
+++ windows/sample.js
@@ -1,13 +1,12 @@
-
 {
-window: "sample",
-items: [
+    "window": "sample",
+        "items": [
             {
-                id: "home_panel",
-                title: "SAMPLE",
-                width: "col-12",
-                background: "bg-primary-reverse",
-                isCard: true,
+                "id": "home_panel",
+                "title": "SAMPLE",
+                "width": "col-12",
+                "background": "bg-primary-reverse",
+                "isCard": true,
             }
         ]
-    }
+}
