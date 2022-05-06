8efb413b8daf3f61a43debc9577cc8c3fcfb850e
Luca <luca.biasotto.89@gmail.com>
2022-05-06 20:06:26 +0200

    add metadata

---
+++ metadata.js
@@ -0,0 +1,26 @@
+{
+    application : "ANACLET_SAMPLE",
+    appLogo : "",
+    menu: [
+        {
+            label: 'App',
+            icon: 'pi pi-fw pi-desktop',
+            items: [
+                {
+                    label: 'Apri window',
+                    icon: 'pi pi-fw pi-plus',
+                    command: (event) => {
+                        navigate('window1');
+                    }
+                },
+                {
+                    label: 'Esegui JS',
+                    icon: 'pi pi-fw pi-search',
+                    command: (event) => {
+                        navigate('window2');
+                    }
+                }
+            ]
+        }
+    ]
+};
--- windows/pippo.js
+++ windows/pippo.js
@@ -1,1 +1,1 @@
-{"window":"pippo","items":[{"id":"home_panel","title":"Pippo","width":"col-12","background":"bg-primary-reverse","isCard":true}]}
+{ "window": "pippo", "items": [{ "id": "home_panel", "title": "Pippo", "width": "col-12", "background": "bg-primary-reverse", "isCard": true }] }
